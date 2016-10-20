0dc8c86f2da811a5313a3faada6a07ccf5784403
DESKTOP-I7B4K59\rpk19 <rpk1994@gmail.com>
2016-10-20 20:37:09 +0200

    Creado proyecto, bean y models

---
+++ src/main/java/es/albarregas/models/Multiplicar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.models;
+
+/**
+ *
+ * @author rpk19
+ */
+public class Multiplicar {
+    public float multiplicar(float param1, float param2){
+        return param1*param2;
+    }
+}
---
+++ src/main/java/es/albarregas/models/Sumar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.models;
+
+/**
+ *
+ * @author rpk19
+ */
+public class Sumar {
+    public float sumar(float param1, float param2){
+        return param1+param2;
+    }
+}
